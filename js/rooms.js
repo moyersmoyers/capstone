778f8027eee31b80255e0eb153e0595270820b60
moyersmoyers <139244464+moyersmoyers@users.noreply.github.com>
2023-10-28 12:30:35 +0800

    Update rooms.js

--- js/rooms.js
+++ js/rooms.js
@@ -7,12 +7,7 @@ AOS.init({
     once: true
 });
 
-$('.card-public').click(function(e){
-    e.preventDefault();
-    $('.public-modal').modal('show');
-});
-
-$('.card-private').click(function(e){
+$('.card').click(function(e){
     e.preventDefault();
     $('.private-modal').modal('show');  
 });
