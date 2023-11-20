134bc49c51fd44c6496f1f6ccbc62886c435ebf5
moyersmoyers <139244464+moyersmoyers@users.noreply.github.com>
2023-11-20 19:58:09 +0800

    Update rooms.js

--- js/rooms.js
+++ js/rooms.js
@@ -9,5 +9,17 @@ AOS.init({
 
 $('.card').click(function(e){
     e.preventDefault();
-    $('.private-modal').modal('show');  
+    const trgt = e.target;
+    if(trgt.parentNode.classList.contains("regular") || trgt.parentNode.parentNode.classList.contains("regular")){
+        $('.regular-modal').modal('show');
+    }
+    else if(trgt.parentNode.classList.contains("big") || trgt.parentNode.parentNode.classList.contains("big")){
+        $('.big-modal').modal('show');
+    }
+    else if(trgt.parentNode.classList.contains("public") || trgt.parentNode.parentNode.classList.contains("public")){
+        $('.public-modal').modal('show');
+    }
+    else if(trgt.parentNode.classList.contains("private") || trgt.parentNode.parentNode.classList.contains("private")){
+        $('.private-modal').modal('show');
+    }
 });
